bfff9830d9e1ef3df9d32f858929bbbfd52824c7
Jake Trimble <jatnerd@gmail.com>
2018-11-14 18:37:56 -0800

    Initial Commit

---
+++ SilverKit/SilverKit.h
@@ -0,0 +1,19 @@
+//
+//  SilverKit.h
+//  SilverKit
+//
+//  Created by Jake Trimble on 11/14/18.
+//  Copyright © 2018 Jake Trimble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SilverKit.
+FOUNDATION_EXPORT double SilverKitVersionNumber;
+
+//! Project version string for SilverKit.
+FOUNDATION_EXPORT const unsigned char SilverKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SilverKit/PublicHeader.h>
+
+
